b38f741155ae3cb05562105dd11fedca0366b5c7
Shrey Dangayach <54055303+igshrey01@users.noreply.github.com>
2022-05-09 13:40:02 +0530

    Time: 40 ms (27.36%), Space: 13.6 MB (36.92%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -31,6 +31,25 @@ public:
         return true;
         
     }
+    bool isBipartiteComponentDFS(int n, int par ,vector<vector<int>> &graph, vector<int>& color)     {
+        if(par==-1){
+            color[n] = 0;
+        }
+        else{
+            color[n] = 1-color[par];
+        }
+        
+        for(auto x:graph[n]){
+            if(color[x]==-1){
+                if(!isBipartiteComponentDFS(x,n,graph,color))
+                    return false;
+            }
+            else if(x!=par)
+                if(color[x]!=(1-color[n]))
+                    return false;
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         
         int n = graph.size();
